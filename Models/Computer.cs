9f99a96d8bc88c6969e8aa69734186783615c60d
Onuratasever <onuratasever24@gmail.com>
2024-09-11 14:09:46 +0300

    Model is created according to table in database

---
+++ Models/Computer.cs
@@ -0,0 +1,23 @@
+namespace HelloWorld.Models
+{
+    public class Computer
+    {
+        public int ComputerId {get; set;}
+        public string Motherboard {get; set;} = "";
+        public int? CPUcores {get; set;}
+        public bool HasWifi {get; set;}
+        public bool HasLTE {get; set;}
+        public DateTime ReleaseDate {get; set;}
+        public decimal Price {get; set;}
+        public string VideoCard {get; set;} = "";
+
+        public Computer()
+        {
+            if(CPUcores == null) // If null comes from the database, it will be set to 0.
+            {
+                CPUcores = 0;
+            }
+        }
+        
+    }
+}

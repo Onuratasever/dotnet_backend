e8378ce55a2675c262ec3b83b13de93e71ed7d29
Onuratasever <onuratasever24@gmail.com>
2024-09-12 13:50:22 +0300

    updates

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0304b7ed4f113db1ff04c32f2ed2d99bf110a9a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8014d5e65795751869f08a5674c505261ebd8497")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

5c05c7a9be7b7e105b8ad3ada9f4846e8625b058
Onuratasever <onuratasever24@gmail.com>
2024-09-12 13:49:24 +0300

    some properties replaced with nullable

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -2,12 +2,12 @@ namespace HelloWorld.Models
 {
     public class Computer
     {
-        public int ComputerId {get; set;}
+        public int? ComputerId {get; set;}
         public string Motherboard {get; set;} = "";
         public int? CPUcores {get; set;}
         public bool HasWifi {get; set;}
         public bool HasLTE {get; set;}
-        public DateTime ReleaseDate {get; set;}
+        public DateTime? ReleaseDate {get; set;}
         public decimal Price {get; set;}
         public string VideoCard {get; set;} = "";
 
@@ -16,6 +16,10 @@ namespace HelloWorld.Models
             if(CPUcores == null) // If null comes from the database, it will be set to 0.
             {
                 CPUcores = 0;
+            }
+            if(ComputerId == null) // If null comes from the database, it will be set to 0.
+            {
+                ComputerId = 5;
             }
         }
         
